8f285363c459c24c94d2848dbee0e8cc93b0f13e
Lautaro <lautaro0121@gmail.com>
2023-04-10 09:33:41 -0300

    cv

--- src/components/FrontPage/FrontPage.tsx
+++ src/components/FrontPage/FrontPage.tsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 import { Typewriter } from "react-simple-typewriter"
 import './Frontpage.css'
 import photo from '../../Images/home-main.svg'
-import curricuCV from '../../Images/Robles Lautaro CV.pdf'
+import curricuCV from '../../Images/Robles-Lautaro CV.pdf'
 
 
 export default function Frontpage ({dataLanguage}:any) {
@@ -54,7 +54,7 @@ export default function Frontpage ({dataLanguage}:any) {
                 </span>
 
                 <div className="flex justify-center align-center pt-20">
-                    <a href={curricuCV} target="_blank" 
+                    <a href={curricuCV} target="_blank"
                     rel="noreferrer">
                         <button 
                         className={
--- src/components/Navbar/Navbar2.jsx
+++ src/components/Navbar/Navbar2.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import {Link} from 'react-scroll'
 import "./Navbar.css";
-import curricuCV from '../../Images/Robles Lautaro CV.pdf'
+import curricuCV from '../../Images/Robles-Lautaro CV.pdf'
 
 
 export default function Navbar2 ({datalang, rendertest}){
